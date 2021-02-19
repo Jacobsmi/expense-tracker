597a7862fb152377ff115abcc43d34372cc33bea
Michael Jacobs <jacobsmi@merrimack.edu>
2021-02-19 14:12:18 -0500

    Styling for add expense form is good

--- pages/addexpense.js
+++ pages/addexpense.js
@@ -2,10 +2,18 @@ import Sidebar from '../components/sidebar'
 import styles from '../styles/AddExpense.module.css'
 
 const AddExpense = () =>{
+    const submitClick = () =>{
+
+    }
     return(
-        <div id='AddExpense'>
+        <div id={styles.AddExpense}>
             <Sidebar active='add'></Sidebar>
             <div id={styles.Title}>Add an Expense</div>
+            <div id={styles.FormContainer}>
+                <label className={styles.FormLabel}>Name:</label><input className={styles.FormInput}></input><br />
+                <label className={styles.FormLabel}>Amount:</label><input className={styles.FormInput}></input><br />
+                <button id={styles.FormButton} onClick={submitClick}>Add</button>
+            </div>
         </div>
     )
 }
